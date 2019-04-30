b9d892aafc0a748b3725dd81969bab0eed5393f2
xica369 <761@holbertonschool.com>
2019-04-30 02:22:32 +0000

    T8: code that create a new sentence

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str.split()
+print(str[5], str[6], str[12], str[0])
