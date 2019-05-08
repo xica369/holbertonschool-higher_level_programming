92b28bb3a3d75e28dd606e3b32f04f8e20ee5e27
xica369 <761@holbertonschool.com>
2019-05-08 23:20:41 +0000

    T14: file of function that prints some basic info about Python lists

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *print_python_list_info - prints some basic info about Python lists
+ *
+ *@p: is a pointer to python object
+ */
+void print_python_list_info(PyObject *p)
+{
+
+}
