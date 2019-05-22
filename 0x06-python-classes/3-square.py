5ae08d4fa7d3283142f7b76b0b7b511a4defc4f7
xica369 <761@holbertonschool.com>
2019-05-22 20:47:18 +0000

    T3: add message of error

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,5 +6,6 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+
     def area(self):
         return self.__size * self.__size
