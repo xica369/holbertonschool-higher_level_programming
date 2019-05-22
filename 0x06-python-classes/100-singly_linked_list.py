d4e5b2832f92bc9b2fe33084d2b5ce9b2d9c47bb
xica369 <761@holbertonschool.com>
2019-05-22 05:15:22 +0000

    T7

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,9 @@
+!/usr/bin/python3
+class Node:
+    def __init__(self, data, next_node=None):
+    def data(self):
+    def data(self, value):
+        if type(data) != int:
+            raise TypeError
+    def next_node(self):
+    def next_node(self, value):
