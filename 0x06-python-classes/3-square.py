12a0baa070d0433386b44090a65037342474af06
xica369 <761@holbertonschool.com>
2019-05-22 04:44:35 +0000

    T3: def area(self)

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) != int:
+            raise TypeError
+            print("size must be an integer")
+        if size < 0:
+            raise ValueError
+            print("size must be >= 0")
+    def area(self):
+        return self.__size * self.__size
