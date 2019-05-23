3a87577c8a3656c6db7b10f8ee60d624cfa043ed
xica369 <761@holbertonschool.com>
2019-05-23 03:07:15 +0000

    T6:  that prints in stdout the square with the character #, with tupla

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,13 +1,42 @@
 #!/usr/bin/python3
 class Square:
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
     def size(self, value):
-        if type(size) != int:
-            raise TypeError
-            print("size must be an integer")
-        if size < 0:
-            raise ValueError
-            print("size must be >= 0")
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
     def area(self):
         return self.__size * self.__size
+
+    @property
+    def position(self):
+        return self.__position
+
+    @size.setter
+    def position(self, value):
+        if type(value) != tuple and len(value) == 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value[0]) is not int and type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 and value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                print(' ' * self.__position[0] + '#' * self.__size)
