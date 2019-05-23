b16154dd74c306c2f3dff7493c904a9da6b875fa
xica369 <761@holbertonschool.com>
2019-05-23 00:25:23 +0000

    T5: prints in stdout the square with the character #

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -2,12 +2,25 @@
 class Square:
     def __init__(self, size=0):
         self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
     def size(self, value):
-        if type(size) != int:
-            raise TypeError
-            print("size must be an integer")
-        if size < 0:
-            raise ValueError
-            print("size must be >= 0")
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
     def area(self):
         return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print('#' * self.__size)
