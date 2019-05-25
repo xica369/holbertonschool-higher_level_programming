671a36b7653fdba9789516eea52d732d1f186a60
xica369 <761@holbertonschool.com>
2019-05-25 04:37:53 +0000

    T5: add comments to file unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
+"""Module to test the function max_integer"""
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
+
+
     def test_max_integer_output(self):
+        """Function to test the output the function max_integer"""
+
         self.assertAlmostEqual(max_integer([1, 3, 4, 2]), 4)
         self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4)
         self.assertAlmostEqual(max_integer([-1, -2, -3, -4]), -1)
         self.assertAlmostEqual(max_integer([]), None)
 
     def test_max_integer_input(self):
+        """Function to test the input the function max_integer"""
+
         self.assertRaises(TypeError, max_integer, 123)
         self.assertRaises(TypeError, max_integer, True)
