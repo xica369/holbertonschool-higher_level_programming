c395e5c5c07361e11bed390686d7edd4d8a6f057
xica369 <761@holbertonschool.com>
2019-05-28 00:20:03 +0000

    T2: class Rectangle that defines a rectangle and return the perimeter and area

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            TypeError("width must be an integer")
+        if value < 0:
+            ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            TypeError("height must be an integer")
+        if value < 0:
+            ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        return self.__height * 2 + self.width * 2
