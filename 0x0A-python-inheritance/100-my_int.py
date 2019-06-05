8ae460618d1e2295a58f00eddab3ac8d53cb0d80
xica369 <761@holbertonschool.com>
2019-06-05 03:47:42 +0000

    T12: again

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,5 +3,8 @@ class MyInt(int):
     def __init__(self, value):
         self.num = value
 
-    def __eq__(self, val):
-        return self.val
+    def __eq__(self, other):
+        return self.num != other
+
+    def __ne__(self, other):
+        return self.num == other
