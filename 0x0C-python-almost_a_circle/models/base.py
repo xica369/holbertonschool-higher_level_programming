da94277133aeae75b16d9a3ff5028857ee65cfbc
xica369 <761@holbertonschool.com>
2019-06-11 23:23:55 +0000

    T15: update class base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This is the class Base"""
+from json import dumps
 
 
 class Base:
@@ -8,9 +9,16 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """function init"""
+        """init function"""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """to_json_string function"""
+        if list_dictionaries is None or is not list_dictionaries:
+            return "[]"
+        return dumps(list_dictionaries)
