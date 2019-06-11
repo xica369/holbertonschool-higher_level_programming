1b8ce5c48629340956d0c368bbf1399494a4df39
xica369 <761@holbertonschool.com>
2019-06-11 16:15:57 +0000

    T7: Rectangle instance with the character # by taking care of x and y

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -80,10 +80,14 @@ class Rectangle(Base):
 
     def display(self):
         i = 0
+        j = 0
+        for j in range(self.__y):
+            print()
         for i in range(self.__height):
-            print('#' * self.__width)
+            print(" " * self.__x + '#' * self.__width)
 
     def __str__(self):
         return ("[Rectangle] ({}) {}/{} - {}/{}".
                   format(self.id, self.__x, self.__y, self.__width,
                          self.__height))
+
