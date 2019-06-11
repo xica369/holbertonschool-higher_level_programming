f7605ac13c67af1ff070dded5b1de52911e015bc
xica369 <761@holbertonschool.com>
2019-06-11 03:38:33 +0000

    T6: class Rectangle with  __str__ method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -82,3 +82,8 @@ class Rectangle(Base):
         i = 0
         for i in range(self.__height):
             print('#' * self.__width)
+
+    def __str__(self):
+        return ("[Rectangle] ({}) {}/{} - {}/{}".
+                  format(self.id, self.__x, self.__y, self.__width,
+                         self.__height))
