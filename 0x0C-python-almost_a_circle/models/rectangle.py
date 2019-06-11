ac623594bd50ea9b856fa8a177d7700a8f95cb9a
xica369 <761@holbertonschool.com>
2019-06-11 19:12:42 +0000

    T8: class Rectangle with method def update(self, *args)

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,6 +5,7 @@ from models.base import Base
 
 class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
+        """function init"""
         if type(width) is not int:
             raise TypeError("width must be an integer")
         if width <= 0:
@@ -29,10 +30,12 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """function getter width"""
         return self.__width
 
     @width.setter
     def width(self, width):
+        """function setter width"""
         if type(width) is not int:
             raise TypeError("width must be an integer")
         if width <= 0:
@@ -41,10 +44,12 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """function getter height"""
         return self.__height
 
     @height.setter
     def height(self, height):
+        """function setter height"""
         if type(height) is not int:
             raise TypeError("height must be an integer")
         if height <= 0:
@@ -53,10 +58,12 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """function getter x"""
         return self.__x
 
     @x.setter
     def x(self, x):
+        """function setter x"""
         if type(x) is not int:
             raise TypeError("x must be an integer")
         if x < 0:
@@ -65,10 +72,12 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """function getter y"""
         return self.__y
 
     @y.setter
     def y(self, y):
+        """function setter y"""
         if type(y) is not int:
             raise TypeError("y must be an integer")
         if y < 0:
@@ -76,9 +85,11 @@ class Rectangle(Base):
         self.__y = y
 
     def area(self):
+        """function getter area"""
         return self.__width * self.__height
 
     def display(self):
+        """function display"""
         i = 0
         j = 0
         for j in range(self.__y):
@@ -87,7 +98,15 @@ class Rectangle(Base):
             print(" " * self.__x + '#' * self.__width)
 
     def __str__(self):
+        """function str"""
         return ("[Rectangle] ({}) {}/{} - {}/{}".
-                  format(self.id, self.__x, self.__y, self.__width,
-                         self.__height))
+                format(self.id, self.__x, self.__y, self.__width,
+                       self.__height))
 
+    def update(self, *args):
+        """function update all attributes"""
+        ar = ["id", "width", "height", "x", "y"]
+        i = 0
+        for i in range(len(args)):
+            if i < 5:
+                setattr(self, ar[i], args[i])
