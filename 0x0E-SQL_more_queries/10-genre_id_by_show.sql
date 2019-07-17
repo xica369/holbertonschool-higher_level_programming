611d85f033b8e876c669d22f2236a9c3e36b188e
xica369 <761@holbertonschool.com>
2019-07-17 04:43:42 +0000

    T10 again

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,1 +1,2 @@
  -- shows contained in hbtn_0d_tvshows that have at least one genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_shows.id ORDER BY FIELD(tv_shows.title, tv_show_genres.genre_id) ASC;
