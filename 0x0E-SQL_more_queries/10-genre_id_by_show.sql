eaed486393625d6694581dba2dcb1222b837712f
xica369 <761@holbertonschool.com>
2019-07-18 04:17:42 +0000

    T10- with fixed condiction order by two parameters

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
-B -- shows contained in hbtn_0d_tvshows that have at least one genre linked
+-- shows contained in hbtn_0d_tvshows that have at least one genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres
