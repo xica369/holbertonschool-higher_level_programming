c94404c825a21001c9d35794062731e129aca6fc
xica369 <761@holbertonschool.com>
2019-07-19 21:34:34 +0000

    T18: cat 101-not_a_comedy.sql | mysql -hlocalhost -uroot -p hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,1 +1,10 @@
 -- Import the database dump from hbtn_0d_tvshows
+SELECT tv_shows.title
+FROM tv_shows
+WHERE tv_shows.title NOT IN (
+SELECT tv_shows.title
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = "Comedy")
+ORDER BY tv_shows.title ASC;
