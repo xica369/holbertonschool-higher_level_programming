6e737777fc177b5bc15e56781286ab15222ff931
xica369 <761@holbertonschool.com>
2019-07-20 03:43:22 +0000

    T20: lists all genres in the database hbtn_0d_tvshows_rate by their rating

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,1 +1,7 @@
 -- Import the database dump from hbtn_0d_tvshows_rate to your MySQL server
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY tv_genres.name
+ORDER BY rating DESC;
