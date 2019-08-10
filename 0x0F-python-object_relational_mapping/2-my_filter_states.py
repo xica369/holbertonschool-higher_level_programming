94c7308db32a12c9561c08b84096c30d6b22a9ca
xica369 <761@holbertonschool.com>
2019-08-10 23:58:58 +0000

    T2: script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,13 +7,16 @@ if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
-    conect = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+    conect = MySQLdb.connect(host="localhost", port=3306,
+                             user="{}".format(argv[1]),
                              passwd=argv[2], db=argv[3], charset="utf8")
     cursor = conect.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("""SELECT *
+    FROM states
+    WHERE name = "{}"
+    ORDER BY id ASC""".format(argv[4]))
     query_rows = cursor.fetchall()
     for row in query_rows:
-        if row[1] == argv[4]:
-            print(row)
+        print("{}".format(row))
     cursor.close()
     conect.close()
