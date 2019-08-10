8220698c0e7a361b7435997cf41e2fe487edc473
xica369 <761@holbertonschool.com>
2019-08-10 22:39:56 +0000

    T2: script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+script that lists all states from the database hbtn_0e_0_usa that start with N
+"""
+if __name__ == "__main__":
+
+    import MySQLdb
+    from sys import argv
+
+    conect = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                             passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = conect.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cursor.fetchall()
+    for row in query_rows:
+        if row[1] == argv[4]:
+            print(row)
+    cursor.close()
+    conect.close()
