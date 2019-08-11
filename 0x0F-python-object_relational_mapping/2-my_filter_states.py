37a020c04872aef86c00d7637901fc671babef6e
xica369 <761@holbertonschool.com>
2019-08-11 00:03:00 +0000

    T2: fixed

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,8 +7,7 @@ if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
-    conect = MySQLdb.connect(host="localhost", port=3306,
-                             user="{}".format(argv[1]),
+    conect = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                              passwd=argv[2], db=argv[3], charset="utf8")
     cursor = conect.cursor()
     cursor.execute("""SELECT *
