09c47fe6ceceab696805fb598f7a36f89562b170
xica369 <761@holbertonschool.com>
2019-08-11 03:48:33 +0000

    T4: fixed

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
                              passwd=argv[2], db=argv[3], charset="utf8")
     cursor = conect.cursor()
     cursor.execute("""SELECT cities.id, cities.name, states.name
-    FROM states
-    LEFT JOIN cities ON cities.state_id = states.id
+    FROM cities
+    LEFT JOIN states ON cities.state_id = states.id
     ORDER BY cities.id ASC""")
     query_rows = cursor.fetchall()
     for row in query_rows:
