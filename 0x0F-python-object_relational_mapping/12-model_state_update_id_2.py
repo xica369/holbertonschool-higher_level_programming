4fd70fb679724bb7c7fe129604959c21ec00d2cd
xica369 <761@holbertonschool.com>
2019-08-12 02:28:46 +0000

    T12: Update a state

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+All states via SQLAlchemy
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    data = session.query(State).filter(State.id == 2)\
+                               .update({State.name: "New Mexico"},
+                                       synchronize_session='fetch')
+    session.commit()
+    session.close()
