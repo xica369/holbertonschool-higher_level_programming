f9966f77a782947ae78d00980a8d6c5eb6ef7183
xica369 <761@holbertonschool.com>
2019-08-12 04:08:53 +0000

    model class city change state to states

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,7 +4,6 @@ class definition of a State and an instance Base = declarative_base()
 """
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base
 
 
@@ -23,4 +22,4 @@ class City(Base):
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("state.id"), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
