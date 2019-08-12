e1306002610d98b265d6f876af386538286a2ada
xica369 <761@holbertonschool.com>
2019-08-12 03:52:19 +0000

    T14: Cities in state

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+All states via SQLAlchemy
+"""
+from sys import argv
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    data = session.query(State.name, City.id, City.name)\
+                  .select_from(State).join(City, City.state_id == State.id)\
+                                     .order_by(State.id)\
+                                     .all()
+
+    for row in data:
+        print("{}: ({}) {}".format(row[0], row[1], row[2]))
+    session.close()
