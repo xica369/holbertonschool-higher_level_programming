ca708e6829aedd816b279f13352d61f5b8700fb0
xica369 <761@holbertonschool.com>
2019-08-27 04:14:47 +0000

    T1: delete space before the (

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     from urllib import request
     from sys import argv
     with request.urlopen(argv[1]) as response:
-        print (response.info().get('X-Request-Id'))
+        print(response.info().get('X-Request-Id'))
