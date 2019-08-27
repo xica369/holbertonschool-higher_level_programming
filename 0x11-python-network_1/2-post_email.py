b7abfe67e1a025c1e898f3f75c690516e0629132
xica369 <761@holbertonschool.com>
2019-08-27 04:55:00 +0000

    T2: POST an email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" displays the value of the X-Request-Id variable found in the header """
+
+
+if __name__ == "__main__":
+    from urllib import request, parse
+    from sys import argv
+    post_fields = {'email': argv[2]}
+    data = parse.urlencode(post_fields)
+    data = data.encode('utf-8')
+    req = request.Request(argv[1], data)
+    with request.urlopen(req) as response:
+        html = response.read()
+        print(html)
