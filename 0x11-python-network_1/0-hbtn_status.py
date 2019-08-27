8567cb695832d844345a79159aafeb5bcb9898f1
xica369 <761@holbertonschool.com>
2019-08-27 03:42:03 +0000

    T0: What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Python script that fetches https://intranet.hbtn.io/status """
+
+
+if __name__ == "__main__":
+    import urllib.request
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html.content))
+        print("\t- utf8 content: {}".format(html.decode(utf8)))
