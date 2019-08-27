fff5dfcc123a6980e6c7b0acc43b028dbf4b61d4
xica369 <761@holbertonschool.com>
2019-08-27 04:10:58 +0000

    T1: remove .format

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,5 +6,4 @@ if __name__ == "__main__":
     from urllib import request
     from sys import argv
     with request.urlopen(argv[1]) as response:
-        html = response.read()
-        print ("{}".format(response.info().get('X-Request-Id')))
+        print (response.info().get('X-Request-Id'))
