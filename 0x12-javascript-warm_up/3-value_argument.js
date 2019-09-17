3c217e9aaee3bbf4e5fceb82097d24600a9bb0f1
xica369 <761@holbertonschool.com>
2019-09-17 23:28:58 +0000

    T3: prints the first argument passed to it

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// prints the first argument passed to it
+// If no arguments are passed to the script, print No argument
+
+function prints () {
+  if (process.argv[2]) {
+    console.log(process.argv[2]);
+  } else {
+    console.log('No argument');
+  }
+}
+
+prints();
