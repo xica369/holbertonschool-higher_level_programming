e52e69660e2c31e5abc1918e8dbe75b552ebe6a1
xica369 <761@holbertonschool.com>
2019-09-18 04:52:18 +0000

    T11: update

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,19 +3,15 @@
 // If no argument passed, print 0
 // If the number of arguments is 1, print 0
 
-function second_biggest () {
-  const list = [];
-  let cont = 2;
-  if (process.argv.length === 2 || process.argv.length === 3) {
-    console.log(0);
-  } if (process.argv.length > 3) {
-    while (process.argv[cont]) {
-      list.push(parseInt(process.argv[cont]));
-      cont++;
-    }
-    list.sort((a, b) => a - b);
-    console.log(list[list.length - 2]);
+const list = [];
+let cont = 2;
+if (process.argv.length === 2 || process.argv.length === 3) {
+  console.log(0);
+} if (process.argv.length > 3) {
+  while (process.argv[cont]) {
+    list.push(parseInt(process.argv[cont]));
+    cont++;
   }
+  list.sort((a, b) => a - b);
+  console.log(list[list.length - 2]);
 }
-
-second_biggest();
