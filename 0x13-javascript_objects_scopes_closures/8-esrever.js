dcb835bbfcfbdbcd987a71ac4f62b5e6a99133f3
xica369 <761@holbertonschool.com>
2019-09-24 04:36:05 +0000

    T8: fixed: change while to for

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,12 +2,9 @@
 // function that returns the reversed version of a list
 
 exports.esrever = function (list) {
-  let cont = list.length - 1;
   const newList = [];
-  while (cont) {
+  for (let cont = list.length - 1; cont >= 0; cont--) {
     newList.push(list[cont]);
-    cont--;
   }
-  newList.push(list[0]);
   return (newList);
 };
