6443b0ee6c3386309fba98e284d6953e521e4dac
xica369 <761@holbertonschool.com>
2019-09-25 00:25:58 +0000

    T0: script that reads and prints the content of a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// script that reads and prints the content of a file
+// The first argument is the file path
+// The content of the file must be read in utf-8
+// If an error occurred during the reading, print the error object
+
+let fs = require('fs');
+const file = process.argv[2];
+
+fs.readFile(file, 'utf8', (error, datos) => {
+  if (error) {
+    console.log(error);
+  } else {
+  console.log(datos);
+  }
+});
