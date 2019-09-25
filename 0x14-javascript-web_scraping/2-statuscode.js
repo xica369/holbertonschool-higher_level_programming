fc28cc20ce0f1cea89461733ec51779383c49306
xica369 <761@holbertonschool.com>
2019-09-25 01:16:28 +0000

    T2: Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// script that display the status code of a GET request
+// The first argument is the URL to request (GET)
+// The status code must be printed like this: code: <status code>
+
+const request = require('request');
+const url = process.argv[2];
+request(url, function (eror, response, body) {
+  console.log('code:', response.statusCode);
+});
