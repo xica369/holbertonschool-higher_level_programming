deee5ebd06d6b5f1a98e7bb7c9bdf98a194e624d
xica369 <761@holbertonschool.com>
2019-09-25 02:32:16 +0000

    T3: prints the title of a Star Wars movie where the episode number matches a given integer

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// prints the title of a Star Wars movie where the episode num matches given int
+// The first argument is the episode number
+
+const request = require('request');
+const url = 'http://swapi.co/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
