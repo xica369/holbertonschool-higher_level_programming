8029ad6bc13a2ad46136449d380a33dff8ccdcff
xica369 <761@holbertonschool.com>
2019-09-26 17:15:57 -0500

    T0: fixed

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,3 +1,4 @@
 // script that updates the text color of the HTML tag HEADER to red (#FF0000)
 
-const colorH = document.getElementsByTagName("header").style.color="#FF0000";
+const colorH = document.querySelector('header');
+colorH.style.color = '#FF0000';
