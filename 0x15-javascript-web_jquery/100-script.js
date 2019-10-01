54e8d2df41036f071388d47551e173e8f0e27634
xica369 <761@holbertonschool.com>
2019-10-01 17:34:00 -0500

    T10: update the text color No jQuery - document loaded

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+// script that updates the text color of the HTML tag HEADER to red (#FF0000)
+
+document.addEventListener('DOMContentLoaded',function () {
+  const colorH = document.querySelector('header');
+  colorH.style.color = '#FF0000';
+});
