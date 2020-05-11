cc970028e4a51cfe736c3621cc12a5312cb924f0
xica369 <761@holbertonschool.com>
2020-05-11 00:46:09 +0000

    test!

--- 0x08-python-more_classes/test.py
+++ 0x08-python-more_classes/test.py
@@ -3,7 +3,7 @@ Rectangle = __import__('1-rectangle').Rectangle
 
 my_rectangle = Rectangle(2, 4)
 print(my_rectangle.__dict__)
-print("-----")
+
 my_rectangle.width = 10
 my_rectangle.height = 3
 print(my_rectangle.__dict__)
