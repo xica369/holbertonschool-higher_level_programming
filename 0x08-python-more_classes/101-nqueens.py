ae30b44f8af42522e3b49ecd6699d08a791c8921
xica369 <761@holbertonschool.com>
2020-05-11 00:19:10 +0000

    file

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -5,4 +5,15 @@
 import sys
 
 
-print("hola")
+if __name__ == "__main__":
+    length = len(argv)
+    if length != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    if argv[1].isdigit() is False:
+        print("N must be a number")
+        exit(1)
+    N = int(argv[1])
+    if N < 4:
+        print("N must be at least 4")
+        exit(1)
