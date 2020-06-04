23ff4e062703bd267494270ad5bc4cd453a7d9f2
xica369 <761@holbertonschool.com>
2020-06-04 18:12:47 -0500

    code juan

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,42 +1,33 @@
 #!/usr/bin/python3
-""" 0. Log parsing
-Write a script that reads stdin line by line and computes metrics
-"""
-
+'Log Parsing'
 import sys
-import signal
 
-file_size = 0
-http_status = {}
-codes_status = ["200", "301", "400", "401", "403", "404", "405", "500"]
-
-
-def print_logs_formated(file_size, http_status):
-    """print_logs_formated"""
-
-    print("File size: {}".format(file_size))
-    for key in sorted(http_status):
-        print("{}: {}".format(key, http_status[key]))
-
-
-def signal_handler(sig, frame):
-    """signal_handler"""
-
-    print_logs_formated(file_size, http_status)
-
-
-for index, line in enumerate(sys.stdin, 1):
-    if line != "":
-        reverted_splitted_line = line.rstrip().split(" ")
-        if len(reverted_splitted_line) == 9:
-            reverted_splitted_line.reverse()
-            file_size += int(reverted_splitted_line[0])
-            if reverted_splitted_line[1] in codes_status:
-                http_status.setdefault(int(reverted_splitted_line[1]), 0)
-                http_status[int(reverted_splitted_line[1])] += 1
-
+status_list = [200, 301, 400, 401, 403, 404, 405, 500]
+try:
+    total_size = 0
+    final_list = []
+    for index, line in enumerate(sys.stdin, 1):
+        if line:
+            line_split = line.split()
+            if len(line_split) > 2:
+                if line_split[-1].isnumeric() and line_split[-2].isnumeric():
+                    size = line_split[-1]
+                    status = line_split[-2]
+                    total_size += int(size)
+            if len(status) > 0 and int(status) in status_list:
+                final_list.append(int(status))
         if index % 10 == 0:
-            print_logs_formated(file_size, http_status)
+            print('File size: {}'.format(total_size))
+            for i in status_list:
+                if i in final_list:
+                    status_count = final_list.count(i)
+                    print("{}: {}".format(i, status_count))
+except Exception:
+    pass
 
-        signal.signal(signal.SIGINT, signal_handler)
-print_logs_formated(file_size, http_status)
+finally:
+    print('File size: {}'.format(total_size))
+    for i in status_list:
+        if i in final_list:
+            status_count = final_list.count(i)
+            print("{}: {}".format(i, status_count))
